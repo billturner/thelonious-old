4b5cce122ecf9b6714ba228e50a0a7ac8a0ac49d
Bill Turner <billturner@gmail.com>
2009-09-19 00:33:37 -0400

    basic saving working; a few more views

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 # libs
-%w(rubygems sinatra dm-core haml sass rdiscount).each do |lib|
+%w(rubygems sinatra dm-core dm-timestamps haml sass rdiscount).each do |lib|
   require lib
 end
 
 # temporarily here:
-set :environment, (ENV['RACK_ENV'] ? ENV['RACK_ENV'].to_sym : :production)
+set :environment, (ENV['RACK_ENV'] ? ENV['RACK_ENV'].to_sym : :development)
 
 # Set up db logging
 DataMapper::Logger.new(STDOUT, :debug)
@@ -12,11 +12,30 @@ DataMapper::Logger.new(STDOUT, :debug)
 # config & db connection ( + models )
 ['config/settings.rb', 'config/database.rb'].each {|file| require file }
 
+# a few helpers
+helpers do
+
+  def permalink_url(post)
+    "#{BLOG_URL}/#{post.created_at.strftime('%Y/%m')}/#{post.slug}"
+  end
+  
+  def markdown(text)
+    RDiscount.new(text).to_html
+  end
+  
+end
+
 # routing & actions
-get '/' do
-  @page_title = "Weblog Posts"
+get '/' do 
   @posts = Post.all
+  @page_title = "Weblog Posts"
   haml :index
+end
+
+get "/:year/:month/:slug" do
+  @post = Post.first(:slug => params[:slug])
+  @page_title = @post.title
+  haml :view
 end
 
 # stylesheet
@@ -25,8 +44,25 @@ get '/style.css' do
   sass :style
 end
 
-# viewing a post
-get '/post/:id' do
-  @post = Post.first(:slug => params[:id])
-  haml :post
+# Add a new post
+get '/new' do
+  @page_title = "Add Post"
+  @post = Post.new
+  haml :new
+end
+post '/new' do
+  @post = Post.create(params[:post])
+  redirect '/'
+end
+
+# Edit existing post
+get '/edit/:id' do
+  @page_title = "Edit Post"
+  @post = Post.get(params[:id])
+  haml :edit
+end
+post '/edit/:id' do
+  @post = Post.get(params[:id])
+  @post.update(params[:post])
+  redirect '/'
 end
--- config/models.rb
+++ config/models.rb
@@ -1,4 +1,5 @@
 class Post
+  
   include DataMapper::Resource
   property :id,         Serial
   property :title,      String,   :length => 255
@@ -7,23 +8,74 @@ class Post
   property :created_at, DateTime
   property :updated_at, DateTime
   
+  # has n, :taggings
+  # has n, :tags, :through => :taggings
+    
+  after :create, :generate_slug
+
+  private
+    
+    def generate_slug
+      self.update(:slug => "#{title.gsub(/[^a-z0-9]+/i, '-').gsub(/-$/, '').downcase}")
+    end
+    
 end
 
 # automatically create or upgrade the post table
 Post.storage_exists? ? Post.auto_upgrade! : Post.auto_migrate!
 
-# class User
+# class Tagging
+#   
 #   include DataMapper::Resource
 #   property :id,         Serial
-#   property :login,      String
-#   property :password,   Text
-#   property :salt,       Text
+#   property :post_id,    Integer
+#   property :tag_id,     Integer
+#   
+#   belongs_to :post
+#   belongs_to :tag
+#   
+# end
+# 
+# # automatically create or upgrade the post table
+# Tagging.storage_exists? ? Tagging.auto_upgrade! : Tagging.auto_migrate!
+# 
+# class Tag
+#   
+#   include DataMapper::Resource
+#   property :id,         Serial
+#   property :title,      String,   :length => 50
+#   property :slug,       String,   :length => 50
 #   property :created_at, DateTime
 #   property :updated_at, DateTime
+#   
+#   has n, :taggings
+#   has n, :posts, :through => :taggings
+#     
+#   after :create, :generate_slug
+# 
+#   private
+#     
+#     def generate_slug
+#       self.update(:slug => "#{title.gsub(/[^a-z0-9]+/i, '-').gsub(/-$/, '').downcase}")
+#     end
+#     
 # end
+# 
+# # automatically create or upgrade the post table
+# Tag.storage_exists? ? Tag.auto_upgrade! : Tag.auto_migrate!
+
+class User
+  include DataMapper::Resource
+  property :id,         Serial
+  property :login,      String
+  property :password,   Text
+  property :salt,       Text
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
 
 # automatically create or upgrade the user table
-# User.storage_exists? ? User.auto_upgrade! : User.auto_migrate!
+User.storage_exists? ? User.auto_upgrade! : User.auto_migrate!
 
 class Page
   include DataMapper::Resource
