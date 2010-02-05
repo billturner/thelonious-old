97952ca3b7b608b5bfa61cc766a07882b86bd6f0
Bill Turner <billturner@gmail.com>
2010-02-05 11:25:24 -0500

    Forcing test database when testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,14 +6,17 @@ require 'spec/interop/test'
 require 'rack/test'
 require 'factory_girl'
 
-# set test environment
-Sinatra::Base.set :environment, :test
-Sinatra::Base.set :run, false
-Sinatra::Base.set :raise_errors, true
-Sinatra::Base.set :logging, false
-
 # bring in the app file
 require File.join(File.dirname(__FILE__), '..', 'thelonious')
+
+# force test db
+MongoMapper.database = 'thelonious_test'
+
+# set test environment
+Thelonious.set :environment, :test
+Thelonious.set :run, false
+Thelonious.set :raise_errors, true
+Thelonious.set :logging, false
 
 Webrat.configure do |config|
   config.mode = :rack
